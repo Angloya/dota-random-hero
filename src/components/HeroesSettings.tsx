9a8204cc65e6c6a16cceca0074707e70d73d7d3c
Алёна <41994884+Angloya@users.noreply.github.com>
2023-12-02 19:51:19 +0200

    margin was updated

--- src/components/HeroesSettings.tsx
+++ src/components/HeroesSettings.tsx
@@ -110,7 +110,7 @@ export default function HeroesSettings({ settings, changeSettings, reset, filter
             </div>
         </div>}
 
-        <div className='mt-4 flex justify-center'>
+        <div className='mt-12 flex justify-center'>
             <label className="relative inline-flex items-center cursor-pointer">
                 <input onChange={changeOwnPool} type="checkbox" checked={settings.ownPool} className="sr-only peer" />
                 <div className="w-12 h-6 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-blue-600" />
