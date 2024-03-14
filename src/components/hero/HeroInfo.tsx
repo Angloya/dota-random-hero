7505b05fbeb019bc4c29a4f317236d06ac96beab
Алёна <41994884+Angloya@users.noreply.github.com>
2024-03-14 10:06:50 +0200

    images path was updated

--- src/components/hero/HeroInfo.tsx
+++ src/components/hero/HeroInfo.tsx
@@ -18,7 +18,7 @@ export default function HeroInfo({hero}: HeroItemProps) {
     return (
         <div className='w-full md:w-[700px] p-6 bg-zinc-700 rounded flex items-center justify-center flex-col'>
             <h1 className='text-2xl mb-4'>{hero.localizedName}</h1>
-            <Image priority className='rounded' src={`https://api.opendota.com${hero.img}`} alt='hero' width={300} height={50} />
+            <Image priority className='rounded' src={`http://cdn.dota2.com/${hero.img}`} alt='hero' width={300} height={50} />
 
             <ul className='m-4 grid divide-y mb-8'>
                 <HeroInfoItem name='Complexity'>
